f82c8dba40c8c2196ede362af9b725c39d53188c
Natalia Borges <n.borges@pipastudios.com>
2020-12-04 22:32:59 -0300

    First Prototype Example

---
+++ ChargeBumpers/Assets/Script/Camera/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+    private Vector3 cameraPosition;
+
+    private void Awake()
+    {
+        cameraPosition = transform.localPosition;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(target.transform.localPosition);
+
+        Vector3 updatePos = new Vector3(cameraPosition.x,
+            cameraPosition.y,
+            cameraPosition.z + target.transform.localPosition.z);
+
+        transform.localPosition = updatePos;
+    }
+}
---
+++ ChargeBumpers/Assets/Script/Car/CarBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+    [SerializeField] private float currentSpeed = 5f;
+    private bool pressed = false;
+    private bool rotated = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * currentSpeed * verticalInput * Time.deltaTime);
+       
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotated = true;
+            transform.Rotate(new Vector3(0, -1, 0)); ;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0));
+        }
+
+
+    }
+}
