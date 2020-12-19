bf051075d5c12c989817eacccb6b6d6a25a7043e
Natalia Borges <n.borges@pipastudios.com>
2020-12-19 11:51:06 -0300

    remove unused stuff add input system add scriptable object for balance and powerup control

---
+++ ChargeBumpers/Assets/ScriptableObjects/CustomCarBalance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CarBalance", menuName = "CarBalance")]
+public class CustomCarBalance : ScriptableObject
+{
+    [Header("Car")]
+    [SerializeField] private float speed = 0;
+    [SerializeField] private int weight = 0;
+    public float Speed => speed;
+    public int Weight => weight;
+
+}
---
+++ ChargeBumpers/Assets/ScriptableObjects/CustomCarPowerup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CarPowerup", menuName = "CarPowerup")]
+public class CustomCarPowerup : ScriptableObject
+{
+    [Header("Powerup")]
+    [SerializeField] private PowerupType type;
+    public PowerupType Type => type;
+
+    public enum PowerupType
+    {
+        NONE,
+        Turbo,
+        Rocket,
+    }
+}
